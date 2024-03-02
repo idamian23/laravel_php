04371b17c27e7c20d641761ed318b61fa56db93f
idamian <damian.ionut23@yahoo.ro>
2024-03-02 16:39:03 +0200

    14 - blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,20 +9,20 @@
 
 </head>
 <body class="antialiased">
-<?php foreach ($posts as $post) : ?>
-<article>
+@foreach ($posts as $post)
+<article class="mb-4">
     <h1>
-        <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?>
+        <a href="/posts/{{$post->slug}}">
+                {{$post->title}}
         </a>
 
     </h1>
 
     <div>
-            <?= $post->excerpt; ?>
+        {{$post->excerpt}}
     </div>
 </article>
-<?php endforeach; ?>
+@endforeach
 
 </body>
 </html>

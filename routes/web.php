3d8e432328f8d11b013de60c09e51b505ab81d1b
idamian <damian.ionut23@yahoo.ro>
2024-03-11 00:02:55 +0200

    36 - Integrate the Design

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,22 @@ Route::get('/', function () {
         'categories'=> Category::all()
         ]);
     // return
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', ['post' => $post]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', ['posts' => $category->posts]);
-});
+    return view('posts', ['posts' => $category->posts,
+        "currentCategory"=> $category,
+        "categories" => Category::all()
+        ]);
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     //dd($author->username);
 
-    return view('posts', ['posts' => $author->posts]);
+    return view('posts', ['posts' => $author->posts,
+        "categories" => Category::all()]);
 });

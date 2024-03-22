0691c15bae6efc07d698e3e85b1bcbf2e7fdea7b
idamian <damian.ionut23@yahoo.ro>
2024-03-22 21:27:49 +0200

    AUTH

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,7 +23,10 @@ class RegisterController extends Controller
            'password'=>'required|min:7'
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        //log the user
+        auth()->login($user);
 
         return redirect("/")->with('success','Your account has been created!');
 
